a914e97086a955d9028963a850b8e34d8e71e740
xhlulu <xhlperso@gmail.com>
2017-09-30 22:01:47 -0400

    updated with ChatterBot

---
+++ terminal_bot_test.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 30 21:21:24 2017
+
+@author: XingHan
+"""
+from chatterbot import ChatBot
+
+bot = ChatBot(
+    "Terminal",
+    storage_adapter="chatterbot.storage.SQLStorageAdapter",
+    logic_adapters=[
+        "chatterbot.logic.MathematicalEvaluation",
+        "chatterbot.logic.TimeLogicAdapter",
+        "chatterbot.logic.BestMatch"
+    ],
+    input_adapter="chatterbot.input.TerminalAdapter",
+    output_adapter="chatterbot.output.TerminalAdapter",
+    database="../database.db"
+)
+
+print("Type something to begin...")
+
+# The following loop will execute each time the user enters input
+while True:
+    try:
+        # We pass None to this method because the parameter
+        # is not used by the TerminalAdapter
+        bot_input = bot.get_response(None)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
